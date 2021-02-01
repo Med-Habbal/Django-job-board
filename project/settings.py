4343a9d9ab579bbdbbe748adf65cc9364c62ca77
Mohammed Habbal <mohammed.habbal.96@gmail.com>
2021-02-01 08:53:09 +0100

    fin

--- project/settings.py
+++ project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$byl3b&e_!xt!4_@83*_@iiu^+fw(%t5au538*ue^p&ob6eqrf'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
